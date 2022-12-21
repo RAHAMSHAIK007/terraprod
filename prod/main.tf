2509697e28999e954170ab1287800749e24df020
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2022-12-21 21:12:55 +0530

    terra prod commits

---
+++ prod/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_instance" "one" {
+  ami           = "ami-051a81c2bd3e755db"
+  instance_type = "t2.medium"
+  tags = {
+    Name = "raham888"
+  }
+}
+
